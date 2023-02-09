1542790d35dabd15786d6f98ae1f465837ab379c
Jack <jack37feus@hotmail.com>
2023-02-09 14:41:52 +0200

    Started making UK map dashboard based on census 2021 data
    Initial data clean for ONS data

---
+++ UK_Map_App/app.py
@@ -0,0 +1,7 @@
+## App to be used for trialing a map of UK data
+
+import calendar
+import plotly.express as px
+from dash import Dash, html, dcc, Input, Output
+import dash_bootstrap_components as dbc
+
